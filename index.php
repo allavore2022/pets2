b54fb53052a9f366d95cd79935a8245f6d23ee6e
Alisa Llavore <allavore@mail.greenriver.edu>
2021-01-29 17:58:07 -0500

    Added controller file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base Class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//Define a default root (home page)
+$f3->route('GET /', function () {
+ //   echo "My Pets";
+    $view = new Template();
+    echo $view->render('views/pet-home.html');
+});
+
+//Rune fat free
+$f3->run();
